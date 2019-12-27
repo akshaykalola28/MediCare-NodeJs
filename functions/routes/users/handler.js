16cf0e5636d7daf01c8692203031265a95c48f1f
aatish70468 <amp70468@gmail.com>
2019-12-27 11:02:22 +0530

    changes in database, register module, login module

--- functions/routes/users/handler.js
+++ functions/routes/users/handler.js
@@ -16,7 +16,7 @@ let postRegisterHandler = async (req, res, next) => {
         token: null
     };
 
-    var ref = firestore.collection(data.user_type);
+    var ref = firestore.collection('users');
     var imageName = data.user_type + data.phoneNumber;
     var fileNameToStore = `${data.user_type}/${imageName}.jpeg`;
     var userExists;
@@ -31,34 +31,34 @@ let postRegisterHandler = async (req, res, next) => {
         await ref.where('uid', '==', uid).get().then(snapshot => {
             userExists = snapshot.size;
             if (userExists && userExists > 0) {
-                res.json(response(409 , "ER_DUP_ENTRY"));
+                res.json(response(409, "ER_DUP_ENTRY"));
             } else {
                 if (base64str == null || base64str == "") {
                     data['imageURL'] = null;
-                    let userRef = firestore.collection(data.user_type).doc(uid);
+                    let userRef = firestore.collection('users').doc(uid);
                     userRef.set(data).then(() => {
-                        res.send("Register Successfully.");
-                        //res.json(response(200 , "Register Successfully."));
+                        //res.send("Register Successfully.");
+                        res.json(response(200, "Register Successfully."));
                     });
                 } else {
                     var imageURL = imageUpload(fileNameToStore, base64str);
                     imageURL.then(url => {
                         data['imageURL'] = url;
-                        let userRef = firestore.collection(data.user_type).doc(uid);
+                        let userRef = firestore.collection('users').doc(uid);
                         userRef.set(data).then(() => {
-                            res.json(response(200 , "Register Successfully."));
+                            res.json(response(200, "Register Successfully."));
                         });
                     }).catch(error => {
-                        res.json(response(415 , error));
+                        res.json(response(415, error));
                     });
                 }
             }
         }).catch(error => {
-            res.json(400 , error);
+            res.json(400, error);
         });
     }).catch((error) => {
         console.log(error);
-        res.json(response(409 , "Email or Phone Number already exists."));
+        res.json(response(409, "Email or Phone Number already exists."));
     });
 };
 
@@ -67,25 +67,35 @@ let postLoginHandler = async (req, res, next) => {
     var email = req.body.email;
     var password = req.body.password;
     var notificationToken = null;
+    var ref = firestore.collection('users');
 
     firebaseAuth.signInWithEmailAndPassword(email, password).then((record) => {
-        console.log("Token: " + record.user.displayName);
-        res.json(response(200 , "Login Successfull"));
+        console.log("Token: " + record.user.uid);
+        var id = record.user.uid;
+        ref.where('uid', '==', id).get().then(snapshot => {
+            snapshot.forEach(doc => {
+                res.send(doc.data());
+            });
+        }).catch((error) => {
+            console.log(error);
+            res.json(response(402, error));
+        });
+        //res.json(response(200, "Login Successfull"));
     }).catch((error) => {
-        res.json(response(401 , error));
+        res.json(response(401, error));
     });
 };
 
 let postDeleteUserHandler = async (req, res, next) => {
 
     var email = req.body.email;
-    var ref = firestore.collection(req.body.user_type);
+    var ref = firestore.collection('users');
     //Verify IdToken for deleting the user
     var idToken = req.body.idToken;
     auth.verifyIdToken(idToken).then((decodedToken) => {
         var uid = decodedToken.uid;
         console.log("Uid: " + uid);
-        res.json(response(200 , "Token is Verified."));
+        res.json(response(200, "Token is Verified."));
         /*auth.getUserByEmail(email).then((userRecord) => {
             var uid = userRecord.uid;
             auth.deleteUser(uid).then(() => {
@@ -101,7 +111,7 @@ let postDeleteUserHandler = async (req, res, next) => {
             res.json(response(false, false, error));
         });*/
     }).catch((error) => {
-        res.json(response(403 , error));
+        res.json(response(403, error));
     });
 };
 
