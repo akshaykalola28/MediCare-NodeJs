ec43dd2f5d2245bb013789e2f1689b1f82acfc76
aatish70468 <amp70468@gmail.com>
2020-03-02 21:01:21 +0530

    changes in patient that is patient can see his previous records by using id

--- functions/routes/patient/route.js
+++ functions/routes/patient/route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const route = express.Router();
 let { postCheckHistoryHandler } = require('./handler');
 let { verifyToken } = require('./../../verifyToken');
-route.post('/checkHistory', verifyToken, postCheckHistoryHandler);
+route.get('/checkHistory/:patientId', verifyToken, postCheckHistoryHandler);
 module.exports = route;
